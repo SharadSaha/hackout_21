61de5e80104060cedaa6fba8069239bd3b2e4b7e
SharadSaha <76741905+SharadSaha@users.noreply.github.com>
2021-08-29 00:03:59 +0530

    Update About.js

--- app/src/components/pages/About.js
+++ app/src/components/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import HeroSection2 from '../HeroSection2';
 
 export default function About() {
   return(
     <>
-    <h1>ABOUT</h1>
+    <HeroSection2 />
     <Footer />
     </>
   )
