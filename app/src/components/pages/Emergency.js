9c1458416d2e214ce39402c30175017a20d3cec6
SharadSaha <76741905+SharadSaha@users.noreply.github.com>
2021-08-28 14:22:00 +0530

    Add files via upload

---
+++ app/src/components/pages/Emergency.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+export default function Emergency() {
+  return (
+    <>
+    <h1>EMERGENCY</h1>
+    <Footer />
+    </>
+  )
+}
